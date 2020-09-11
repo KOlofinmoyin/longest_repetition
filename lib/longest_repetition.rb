b1cf6015bf1d5d8830ab4c242205613edf8666a9
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-09-11 16:05:01 +0100

    Takes ('aa') and returns ['aa', 2].

--- lib/longest_repetition.rb
+++ lib/longest_repetition.rb
@@ -1,6 +1,6 @@
 def longest_repetition(characters)
   result = []
   return ['',0] if characters.nil? || characters == ""
-  result << characters
-  result << characters.count(characters)
+  characters.inject { |characters| result << characters.count(characters) }
+  # result << characters.count(characters)
 end
