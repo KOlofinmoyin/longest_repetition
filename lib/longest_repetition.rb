81cbc205f5e70d43ce9478f9ea1e20795972383b
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-09-11 15:59:41 +0100

    Takes ('aa') and returns ['aa', 2].

--- lib/longest_repetition.rb
+++ lib/longest_repetition.rb
@@ -1,3 +1,6 @@
 def longest_repetition(characters)
-  return ['',0] if characters.nil? || characters == "" 
+  result = []
+  return ['',0] if characters.nil? || characters == ""
+  result << characters
+  result << characters.count(characters)
 end
--- spec/longest_repetition_spec.rb
+++ spec/longest_repetition_spec.rb
@@ -1,7 +1,13 @@
 require 'longest_repetition'
 
 describe "#longest_repetition" do
-  it "takes ("") and returns ['', 0] " do
+  it "takes ('') and returns ['', 0] " do
     expect(longest_repetition("")).to eq(['',0])
   end
+
+  it "takes ('aa') and returns ['aa', 2] " do
+    expect(longest_repetition("aa")).to eq(['aa',2])
+  end
+
+
 end
